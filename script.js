4980d922cd2c6ef43e541f33cacad5fa2a669385
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-07-02 16:18:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Start toggle menu mobile devices
+
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0px";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+// End toggle menu mobile devices
